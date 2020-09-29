64c818f24c3703a4ee250e599f3cacfd5284b520
Marco Baturan <marco.baturan@gmail.com>
2020-09-29 23:24:08 +0200

    codigo probado

---
+++ voicetranslator.py
@@ -0,0 +1,89 @@
+"""Documentación.
+    
+    Este programa puede reconocer voz y psarla a texto que luego traduce
+    a otro idioma y lo recita. De base es de inglés a francés.
+    Para modificar el idioma sólo hay que cambiar los parametors:
+        - dest=
+        - src=
+        En tl.translator()
+    Modificación del ejercicio indicado en REALPYTHON:
+    https://realpython.com/python-speech-recognition/
+https://pypi.org/project/espeakng/"""
+
+import random
+import time
+from googletrans import Translator
+import speech_recognition as sr
+import espeakng
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+if __name__ == "__main__":
+    tl = Translator()
+
+    # create recognizer and mic instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    # get a random word from the list
+
+    guess = recognize_speech_from_mic(recognizer, microphone)
+    # show the user the transcription
+    origin="{}".format(guess["transcription"])
+    # Traduce de inglés a francés. Lo cambias aquí
+    source='en'
+    destiny='fr'
+    translation=tl.translate(text=origin,src=source,dest=destiny).text
+    print(origin) # Imprime original
+    print(translation) # Imprime resultado
+    speak = espeakng.Speaker() # Instanciamos la voz
+    speak.voice=destiny # Le damos la pronunciación de la traducción
+    speak.say(translation) # Y a escuchar
+
+ 
